a321f22a56c48450c282a0501527f4755a83a461
Rafal Wizen <rafal.wizen@gmail.com>
2025-04-16 18:29:03 +0200

    Add images and icons

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,17 @@
 import { Stack } from "expo-router";
+import {StatusBar} from "react-native";
 import './globals.css';
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+      <>
+        <StatusBar hidden={true}/>
+        <Stack>
+          <Stack.Screen
+              name={"(tabs)"}
+              options={{headerShown: false}}
+          />
+        </Stack>
+      </>
+  );
 }
